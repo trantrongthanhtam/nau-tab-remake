0ce85d5fc4a07019580acccf60ebe4df8c49cbf8
Trần Trọng Thanh Tâm <49970372+trantrongthanhtam@users.noreply.github.com>
2019-09-01 16:29:43 +0700

    creat quicklinks.js file

---
+++ app/js/quicklinks.js
@@ -0,0 +1,15 @@
+const quicklinks = [
+    { id: 'gmail', url: 'https://mail.google.com', icon: 'mdi--gmail', title: 'GMail' },
+    { id: 'gcalendar', url: 'https://calendar.google.com', icon: 'mdi--calendar', title: 'Google Calendar' },
+    { id: 'gdrive', url: 'https://drive.google.com', icon: 'mdi--google-drive', title: 'Google Drive' },
+    { id: 'github', url: 'https://github.com', icon: 'mdi--github-circle', title: 'Github' },
+    { id: 'bitbucket', url: 'https://bitbucket.org', icon: 'mdi--bitbucket', title: 'Bitbucket' },
+    { id: 'trello', url: 'https://trello.com', icon: 'mdi--trello', title: 'Trello' },
+    { id: 'facebook', url: 'https://facebook.com', icon: 'mdi--facebook', title: 'Facebook' },
+    { id: 'twitter', url: 'https://twitter.com', icon: 'mdi--twitter', title: 'Twitter' },
+    { id: 'tuoitre', url: 'http://tuoitre.vn', icon: 'mdi--newspaper', title: 'Tuoitre.vn' },
+    { id: 'vnexpress', url: 'http://vnexpress.net/', icon: 'mdi--newspaper', title: 'VNExpress' },
+    { id: 'thanhnien', url: 'http://thanhnien.vn/', icon: 'mdi--newspaper', title: 'Thanhnien.vn' },
+    { id: 'gphotos', url: 'https://photos.google.com', icon: 'mdi--image', title: 'Google Photos' },
+    { id: 'youtube', url: 'https://youtube.com', icon: 'mdi--youtube-play', title: 'Youtube' },
+];
