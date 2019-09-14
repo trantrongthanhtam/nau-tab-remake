e3e81f5ffcf3d8bc3422f46751d940f0fe69ac5f
Trần Trọng Thanh Tâm <49970372+trantrongthanhtam@users.noreply.github.com>
2019-09-14 10:49:38 +0700

    update Background Img

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,15 @@
 // Update time watch and greetings
 const clockEl = document.querySelector('#clock');
 const greetingEl = document.querySelector('#greeting-text');
+
 setInterval(() => {
     let nowTime = new Date();
     let nowHour = nowTime.getHours();
     let strHour = '' + nowHour;
     let nowMinute = nowTime.getMinutes();
     let strMinute = '' + nowMinute;
+    let nowSecond = nowTime.getSeconds();
+    let strSecond = '' + nowSecond;
     clockEl.textContent = strHour.padStart(2, "0") + ":" + strMinute.padStart(2, "0");
     // check when is the time
     if ((nowHour >= 21) || (nowHour < 5)) {
@@ -42,13 +45,12 @@ settingEl.addEventListener('click', (event) => {
     settingModalEl.classList.toggle('modal--show');
 })
 //clicked close modal button
-settingCloseBtnEl.addEventListener('click', (event) => {
+settingCloseBtnEl.addEventListener('click', () => {
     settingModalEl.classList.remove('modal--show');
 })
 
 //clicked outside of modal menu to collapse it
 bodyEl.addEventListener('click', event => {
-    console.log(event.target.closest('.modal'));
     if (settingModalEl.classList.contains('modal--show') == true) {
         if (event.target.closest('.modal') == null) {
             settingModalEl.classList.remove('modal--show');
@@ -58,8 +60,7 @@ bodyEl.addEventListener('click', event => {
 
 //render quicklinks in nau-tab
 const quickLinksEl = document.querySelector('#quicklinks');
-
-quicklinks.forEach(element => {
+quicklinks.forEach((element, index) => {
     const liEl = document.createElement('li');
     const aEl = document.createElement('a');
     liEl.className = "quick-links__li";
@@ -73,9 +74,194 @@ quicklinks.forEach(element => {
         spanEl.className = "u-serif-text";
         spanEl.textContent = element.id === 'thanhnien' ? "Tn" : element.id === 'vnexpress' ? "vE" : "tt";
         aEl.appendChild(spanEl);
-    } else aEl.classList.add(element.icon);
+    } else { aEl.classList.add(element.icon); }
     liEl.appendChild(aEl);
     quickLinksEl.appendChild(liEl);
 });
 
+const formEl = document.querySelector('#settings-panel');
+const settingQuicklinkEl = document.querySelectorAll('#setting-quicklinks input');
+//saving data in setting panel
+formEl.addEventListener('change', () => {
+    const myData = Array.from(formEl.elements).reduce((data, input) => {
+        if (input.type === 'radio') {
+            data[input.name] = data[input.name] || (input.checked && input.value) || '';
+        } else if (input.type === "checkbox") {
+            data[input.dataset.linkId] = input.checked;
+        }
+        // else if (input.localName === "fieldset") {
+        //     return data;
+        // }
+        return data;
+    }, {});
+    localStorage.setItem('user', JSON.stringify(myData));
+    update();
+});
 
+// on Window loaded:
+window.addEventListener('load', () => {
+    update();
+    if (localStorage.getItem('bg-backup') == null) {
+        localStorage.setItem('bg-backup', `{"url":{"raw":"https://images.unsplash.com/photo-1555117746-a7c449e5d0ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQzOTh9","full":"https://images.unsplash.com/photo-1555117746-a7c449e5d0ff?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjQzOTh9","regular":"https://images.unsplash.com/photo-1555117746-a7c449e5d0ff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjQzOTh9","small":"https://images.unsplash.com/photo-1555117746-a7c449e5d0ff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjQzOTh9","thumb":"https://images.unsplash.com/photo-1555117746-a7c449e5d0ff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjQzOTh9","custom":"https://images.unsplash.com/photo-1555117746-a7c449e5d0ff?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1920&fit=crop&ixid=eyJhcHBfaWQiOjQzOTh9"},"author":"Ethan Dow","location":"SF Bay Area, CA"}`);
+    }
+    if (formEl[4].value !== "user") {
+        changeBgImgOnCondition();
+        console.log(formEl);
+    }
+});
+
+function changeBgImgOnCondition() {
+    const getBgInfo = localStorage.getItem('bg-info');
+    if (getBgInfo && (timePass() < 3600000)) {
+        setBgImg(getBgInfo);
+    } else {
+        let getBgBackupInfo = localStorage.getItem('bg-backup');
+        setBgImg(getBgBackupInfo);
+        localStorage.setItem('bg-info', getBgBackupInfo);
+        fetchNewPhoto(new Date());
+    }
+}
+
+function timePass() {
+    const nowTime = new Date();
+    const checkTime = localStorage.getItem('Last Time Open');
+    let timeHasPass;
+    if (checkTime) {
+        timeHasPass = nowTime - checkTime;
+    } else timeHasPass = 0;
+    localStorage.setItem('Last Time Open', Date.parse(nowTime));
+    return timeHasPass = 5000000;
+}
+
+function update() {
+    //try to read localStorage
+    let storedUser;
+    try {
+        storedUser = JSON.parse(localStorage.getItem('user'));
+    } catch (error) {
+        console.error(error);
+    }
+    //get list icon of quicklinks
+    const quickLinksIconEl = document.querySelectorAll('#quicklinks li');
+    for (const list of quickLinksIconEl) {
+        let listId = list.id;
+        const selectQuicklinkEl = document.querySelector(`#setting-quicklinks input[data-link-id = ${listId}]`);
+        if ((storedUser != null) && (storedUser[listId] === true)) {
+            list.classList.remove('u-hidden');
+            selectQuicklinkEl.checked = true;
+        } else {
+            selectQuicklinkEl.checked = false;
+            list.classList.add('u-hidden');
+        }
+    }
+}
+
+//fetch unsplash nau-tab background
+const unsplashAPI = 'https://api.unsplash.com/photos/random';
+const collectionsByPeriod = {
+    day: [1507483],
+    night: [4747434],
+    dawn: [4748158],
+    dusk: [4748158],
+};
+
+/**
+ * Fetch Unplash random image
+ *
+ * @param {string} period Period of the day to change appropriate collection (day, night, dawn, dusk)
+ * @return {Promise}        the request resolving promise object
+ */
+
+
+function queryString(params) {
+    return Object.keys(params).map(k => {
+        let val = params[k];
+        // join array values with comma
+        if (Array.isArray(val)) {
+            val = val.join(',');
+        }
+
+        return `${encodeURIComponent(k)}=${encodeURIComponent(val)}`;
+    }).join('&');
+}
+
+
+function fetchUnsplash(period = 'day') {
+    const params = {
+        orientation: 'landscape',
+        w: 1920,
+        // my collection with curated photos, refer to https://unsplash.com/@trongthanh/collections
+        collections: collectionsByPeriod[period] || collectionsByPeriod.day,
+    };
+
+    // NOTE: the client id belongs to Nau-Tab only, please request your own Application at Unsplash
+    const requiredHeaders = new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Client-ID 01334b1872fbd0bda3f4f3ba44f0693213cf06f682b81079f8adf802fb89993b',
+    });
+
+    const url = `${unsplashAPI}?${queryString(params)}`;
+    console.log('fetch url:', url);
+    const req = new Request(url, {
+        method: 'GET',
+        headers: requiredHeaders,
+    });
+
+    return fetch(req)
+        .then(response => {
+            console.log('response.status:', response.status);
+            if (response.ok) {
+
+                return response.json();
+
+                // result will be JSON object
+            }
+
+            return response.json();
+            // Even response is 4**, we still receive json object describe error
+        })
+        .catch(err => {
+            console.log('Errors:', err);
+            return err;
+        });
+}
+
+function getDayPeriod(timeValue) {
+    let time = timeValue;
+    if (!(time instanceof Date)) {
+        time = new Date(time);
+    }
+    const hours = time.getHours();
+
+    if (hours < 5) {
+        return 'night';
+    } else if (hours < 7) {
+        return 'dawn';
+    } else if (hours < 18) {
+        return 'day';
+    } else if (hours < 20) {
+        return 'dusk';
+    }
+
+    // else
+    return 'night';
+}
+async function fetchNewPhoto(time) {
+    // const period = getDayPeriod(1549029600000); // this value to test night time
+    const period = getDayPeriod(time);
+    const json = await fetchUnsplash(period);
+    const bgInfo = {
+        'url': json.urls,
+        'author': json.user.name,
+        'location': json.user.location,
+    };
+    localStorage.setItem('bg-backup', JSON.stringify(bgInfo));
+}
+
+function setBgImg(bginfo) {
+    const bgInfoObj = JSON.parse(bginfo);
+    const wallPaperEl = document.querySelector('#wallpaper');
+    wallPaperEl.style.backgroundImage = `url(${bgInfoObj.url.regular})`;
+    wallPaperEl.style.backgroundSize = `cover`;
+}
--- app/js/quicklinks.js
+++ app/js/quicklinks.js
@@ -7,9 +7,11 @@ const quicklinks = [
     { id: 'trello', url: 'https://trello.com', icon: 'mdi--trello', title: 'Trello' },
     { id: 'facebook', url: 'https://facebook.com', icon: 'mdi--facebook', title: 'Facebook' },
     { id: 'twitter', url: 'https://twitter.com', icon: 'mdi--twitter', title: 'Twitter' },
+    { id: 'gplus', url: 'https://plus.google.com', icon: 'mdi--google-plus', title: 'Google +' },
     { id: 'tuoitre', url: 'http://tuoitre.vn', icon: 'mdi--newspaper', title: 'Tuoitre.vn' },
     { id: 'vnexpress', url: 'http://vnexpress.net/', icon: 'mdi--newspaper', title: 'VNExpress' },
     { id: 'thanhnien', url: 'http://thanhnien.vn/', icon: 'mdi--newspaper', title: 'Thanhnien.vn' },
     { id: 'gphotos', url: 'https://photos.google.com', icon: 'mdi--image', title: 'Google Photos' },
     { id: 'youtube', url: 'https://youtube.com', icon: 'mdi--youtube-play', title: 'Youtube' },
+    { id: 'naujukebox', url: 'https://jukebox.naustud.io', icon: 'mdi--play', title: 'Nau Jukebox' },
 ];
